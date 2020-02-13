8d7759f2c2eea3a09774ccd44c93f342bf3034f7
theallegrarr <nurudeen.amedu@gmail.com>
2020-02-13 15:36:12 +0100

    more classes added

--- src/player.py
+++ src/player.py
@@ -1,5 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item import Item
+
 class Player:
-  def __init__(self, room):
+  def __init__(self, name, room, inventory = None):
+    self.name = name
     self.room = room
+    self.inventory = inventory
+  
+  def getItem(self, name):
+    self.inventory.append(Item(name))
+  
+  def listItems(self):
+    if self.inventory == None:
+      return 'no items collected by this player \n'
+    else:
+      for i in self.inventory:
+        print(i)
--- src/room.py
+++ src/room.py
@@ -1,6 +1,33 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-class Room:
-  def __init__(self, name, desc):
-    self.name: name
-    self.description: desc
+from item import Item
+from shutil import get_terminal_size
+
+cols, rows = get_terminal_size()
+
+class NewRoom:
+  def __init__(self, name, description,
+                 n_to=None, s_to=None,
+                 e_to=None, w_to=None,
+                 visible_items=[]):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+        self.visible_items = visible_items
+  def __directions__(self):
+      divider = "_" * (cols - 1)
+      return    divider + "\n" \
+              + f"{self.n_to.name if self.n_to else ' '}".center(cols) + "\n" \
+              + f"{self.w_to.name if self.w_to else ' '}".ljust(cols) \
+              + f"<-- You -->".center(cols) \
+              + f"{self.e_to.name if self.e_to else ' '}".rjust(cols) + "\n" \
+              + f"{self.s_to.name if self.s_to else ' '}".center(cols) + "\n" \
+              + divider
+  def __str__(self):
+    return f'{self.name} {self.n_to} {self.s_to}'
+  
+  def addItem(self, item):
+    self.visible_items = self.visible_items.append(item)
